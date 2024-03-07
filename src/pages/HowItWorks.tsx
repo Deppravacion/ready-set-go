e11b1fe0e3b096ee4ebb1a957132c4679b84603e
Tyson <deppravacion@gmail.com>
2024-03-06 20:13:19 -0700

    add how it works data

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -1,15 +1,55 @@
+type CardProps = {
+  cardTitle: string;
+  cardBody: string;
+};
+
+const Card: React.FC<CardProps> = ({ cardTitle, cardBody }) => {
+  return (
+    <div className='card shadow-sm bg-gray-200'>
+      <div className='container mx-auto p-5 bg-cyan-600 rounded-md'>
+        <h2 className='text-lg'>{cardTitle}</h2>
+      </div>
+      <div className='card-body'>{cardBody}</div>
+    </div>
+  );
+};
+
 export const HowItWorks = () => {
+  const cardBodyData = [
+    {
+      cardTitle: "Step 1",
+      cardBody: "Create and account or use the lite version as guest.",
+    },
+    {
+      cardTitle: "Step 2",
+      cardBody:
+        "Select your storage tpye: Camper/Boat, Vehicle, or Storage Unit.",
+    },
+    {
+      cardTitle: "Step 3",
+      cardBody: "Add your items with photos and descriptions.",
+    },
+    {
+      cardTitle: "Step 4",
+      cardBody: "Check stores and locate items fast",
+    },
+  ];
+
   return (
     <div className='card w-96 bg-base-100 shadow-xl'>
       <div className='container mx-auto p-20 bg-cyan-600 rounded-md'>
         <h2 className='text-lg'>How It Works</h2>
       </div>
       <div className='card-body'>
-        <h2 className='card-title'>Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className='card-actions justify-end'>
-          <button className='btn btn-primary'>Buy Now</button>
-        </div>
+        {cardBodyData.map((card, index) => {
+          return (
+            <Card
+              key={index}
+              cardTitle={card.cardTitle}
+              cardBody={card.cardBody}
+            />
+          );
+        })}
       </div>
     </div>
   );

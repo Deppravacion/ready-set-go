ca6ced0eeec74fda40bd0c216d17183a472b8ded
Tyson <deppravacion@gmail.com>
2024-03-21 09:50:05 -0600

    create store update

--- src/pages/CreateStore.tsx
+++ src/pages/CreateStore.tsx
@@ -9,7 +9,39 @@ export const CreateStore = () => {
           <h2 className='text-lg'>{title}</h2>
           <h2 className='text-md'>{subTitle}</h2>
         </div>
-        <div className='card-body'></div>
+        <div className='card-body'>
+          {/* ******** */}
+          <form className='card-body'>
+            <div className='form-control'>
+              {/* Daisy avatar */}
+
+              {/* Daisy avatar */}
+              <label className='label'>
+                <span className='label-text'>Name</span>
+              </label>
+              <input
+                type='text'
+                placeholder='name'
+                className='input input-bordered'
+                required
+              />
+            </div>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Add Item</span>
+              </label>
+              <input
+                type='text'
+                placeholder='add first item'
+                className='input input-bordered'
+                required
+              />
+            </div>
+            <div className='form-control mt-6'>
+              <button className='btn btn-primary'>Next</button>
+            </div>
+          </form>
+        </div>
         <div className='flex justify-between'>
           <button className='btn btn-outline rounded-none btn-warning px-2'>
             Logout

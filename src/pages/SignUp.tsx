7550ec8761adc804aa9be6d1ecdb34cb49d93b14
Tyson <deppravacion@gmail.com>
2024-05-09 22:42:22 -0600

    add: headers copy to signup page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -12,65 +12,73 @@ export const SignUp = () => {
   };
   return (
     <div className='hero min-h-screen'>
-      <div className='card shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
-        <form className='card-body'>
-          <div className='form-control'>
-            {/* Daisy avatar */}
-            <div className='avatar justify-center'>
-              <div className='w-36 mask mask-squircle'>
-                <img src={profileImage} />
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <div className='text-center lg:text-left'>
+          <h1 className='text-5xl font-bold'>Sign Up Now!</h1>
+          <p className='py-6'>
+            Welcome to READY-SET-GO! Please sign-up to create your account.
+          </p>
+        </div>
+        <div className='card shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+          <form className='card-body'>
+            <div className='form-control'>
+              {/* Daisy avatar */}
+              <div className='avatar justify-center'>
+                <div className='w-36 mask mask-squircle'>
+                  <img src={profileImage} />
+                </div>
               </div>
+              {/* Daisy avatar */}
+              <label className='label'>
+                <span className='label-text'>Email</span>
+              </label>
+              <input
+                type='email'
+                placeholder='email'
+                className='input input-bordered'
+                required
+              />
             </div>
-            {/* Daisy avatar */}
-            <label className='label'>
-              <span className='label-text'>Email</span>
-            </label>
-            <input
-              type='email'
-              placeholder='email'
-              className='input input-bordered'
-              required
-            />
-          </div>
-          <div className='form-control'>
-            <label className='label'>
-              <span className='label-text'>Password</span>
-            </label>
-            <input
-              type='password'
-              placeholder='password'
-              className='input input-bordered'
-              required
-            />
-          </div>
-          <div className='form-control'>
-            <label className='label'>
-              <span className='label-text'>Password</span>
-            </label>
-            <input
-              type='password'
-              placeholder='password'
-              className='input input-bordered'
-              required
-            />
-          </div>
-          <div className='form-control mt-6 flex gap-1'>
-            <button
-              type='submit'
-              className='btn btn-info'
-              onClick={() => handleSignUp()}
-            >
-              Sign Up
-            </button>
-            <button
-              type='submit'
-              className='btn btn-primary'
-              onClick={() => goToSignIn()}
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Password</span>
+              </label>
+              <input
+                type='password'
+                placeholder='password'
+                className='input input-bordered'
+                required
+              />
+            </div>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Password</span>
+              </label>
+              <input
+                type='password'
+                placeholder='password'
+                className='input input-bordered'
+                required
+              />
+            </div>
+            <div className='form-control mt-6 flex gap-1'>
+              <button
+                type='submit'
+                className='btn btn-info'
+                onClick={() => handleSignUp()}
+              >
+                Sign Up
+              </button>
+              <button
+                type='submit'
+                className='btn btn-primary'
+                onClick={() => goToSignIn()}
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );

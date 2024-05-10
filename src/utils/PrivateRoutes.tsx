f7f26729f7c343ed54095f9d87ec95634746fa7f
Tyson <deppravacion@gmail.com>
2024-05-09 22:43:06 -0600

    remove: comments

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,15 +1,6 @@
-// import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 export const PrivateRoutes = () => {
-  // const [auth, setAuth] = useState<string | null>(null);
-  // console.log("token:", auth);
-  // useEffect(() => {
-  //   const key = sessionStorage.getItem("user") ?? null;
-  //   setAuth(key);
-  //   console.log("key:", key);
-  //   console.log(sessionStorage.getItem("user"));
-  // }, []);
   const auth = sessionStorage.getItem("user") ?? null;
   return auth ? <Outlet /> : <Navigate to='/signin' />;
 };

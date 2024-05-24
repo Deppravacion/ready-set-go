8262c222d8d4a58138aa63d4f155f183a420d525
Tyson <deppravacion@gmail.com>
2024-05-24 00:07:37 -0600

    feat: Add functionality to create a new store

--- src/providers/AppContext.tsx
+++ src/providers/AppContext.tsx
@@ -1,26 +1,70 @@
-import { useContext, useState, createContext } from "react";
+import { useContext, useState, createContext, useEffect } from "react";
 import {
   AppContextTypes,
-  FavoritesType,
-  ItemsType,
+  // FavoritesType,
+  // ItemsType,
   StoresType,
 } from "../types/AppTypes";
+import { toast } from "react-toastify";
+import { createStore, getStoresByUserId } from "../api/stores/api-stores";
 
 export const AppContext = createContext({} as AppContextTypes);
 export const AppProvider = ({ children }: { children: JSX.Element }) => {
   const [stores, setStores] = useState<StoresType[] | null>(null);
-  const [items, setItems] = useState<ItemsType[] | null>(null);
-  const [favorites, setFavorites] = useState<FavoritesType[] | null>(null);
+  // const [items, setItems] = useState<ItemsType[] | null>(null);
+  // const [favorites, setFavorites] = useState<FavoritesType[] | null>(null);
+
+  const handleAddStore = async (name: string, userId: string) => {
+    // can add checks to see if the store name already exists.
+    const newStore = {
+      id: Math.random().toString(),
+      name,
+      userId,
+    };
+    try {
+      createStore(newStore);
+    } catch (error) {
+      console.error(error);
+      toast.error("Error creating store");
+    }
+  };
+
+  const handleAddItem = async () => {};
+
+  const handleGetUserStores = async (userId: string) => {
+    try {
+      // const response = await fetch(
+      //   `http://localhost:3004/stores?userId=${userId}`
+      // );
+      // if (!response.ok) {
+      //   throw new Error("Error fetching stores");
+      // }
+      // const data = await response.json();
+      // const userStores = data.filter(
+      //   (store: StoresType) => store.userId === userId
+      // );
+      // setStores(userStores);
+      getStoresByUserId(userId);
+    } catch (error) {
+      console.error(error);
+      toast.error("Error fetching stores");
+    }
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <AppContext.Provider
       value={{
-        stores,
-        setStores,
-        items,
-        setItems,
-        favorites,
-        setFavorites,
+        // stores,
+        // setStores,
+        handleAddStore,
+        handleGetUserStores,
+        handleAddItem,
+        // items,
+        // setItems,
+        // favorites,
+        // setFavorites,
       }}
     >
       {children}
--- src/types/AppTypes.ts
+++ src/types/AppTypes.ts
@@ -1,20 +1,23 @@
-export type AppContextTypes = {
-  stores: StoresType[] | null;
-  setStores: (stores: StoresType[] | null) => void;
-  items: ItemsType[] | null;
-  setItems: (items: ItemsType[] | null) => void;
-  favorites: FavoritesType[] | null;
-  setFavorites: (favorites: FavoritesType[] | null) => void;
+// export type AppContextTypes = {
+//   stores: StoresType[] | null;
+//   setStores: (stores: StoresType[] | null) => void;
+//   items: ItemsType[] | null;
+//   setItems: (items: ItemsType[] | null) => void;
+//   favorites: FavoritesType[] | null;
+//   setFavorites: (favorites: FavoritesType[] | null) => void;
   // ** add these methods next **
-  // handleAddStore: (name: string, userId: string) => Promise<void>;
-  // handleAddItem: (
-  //   name: string,
-  //   image: string,
-  //   description: string,
-  //   quantity: string,
-  //   minQuantity: string,
-  //   storeId: string
-  // ) => Promise<void>;
+  handleAddStore: (name: string, userId: string) => Promise<void>;
+  handleGetUserStores: (userId: string) => Promise<void>;
+
+  handleAddItem: (
+    id: string,
+    name: string,
+    image: string,
+    description: string,
+    quantity: string,
+    minQuantity: string,
+    storeId: string
+  ) => Promise<void>;
   // handleAddFavorite: (
   //   userId: string,
   //   itemId: string,

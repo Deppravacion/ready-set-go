78c74b14810d66c4ecaaf5a8575ac4c72de914d2
Tyson <deppravacion@gmail.com>
2024-05-26 21:43:36 -0600

    refactor: user store rendering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,62 @@
-import { useNavigate } from "react-router-dom";
+import { FavoritesType, ItemsType, StoresType } from "../types/AppTypes";
 import { useAuthProvider } from "../providers/AuthContext";
-const title: string = "Ready Set Go!";
-// const subTitle: string = " Your Home Page";
+import { useAppProvider } from "../providers/AppContext";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getItemsByStoreId } from "../api/items/api-items";
 
 type StoreCardProps = {
-  cardTitle: string;
-  cardBody: string[];
+  store: StoresType;
 };
 
-const StoreCard: React.FC<StoreCardProps> = ({ cardTitle, cardBody }) => {
+const StoreCard: React.FC<StoreCardProps> = ({ store }) => {
+  const [items, setItems] = useState<ItemsType[] | null>(null);
+
+  useEffect(() => {
+    getItemsByStoreId(store.id).then((storeItems: ItemsType[]) => {
+      setItems(storeItems);
+    });
+  }, []);
+
   return (
     <div className='card shadow-sm bg-slate-200'>
       <div className='container mx-auto p-1 bg-slate-400 rounded-none'>
-        <h2 className='text-lg'>{cardTitle}</h2>
+        <h2 className='text-lg'>{store.name}</h2>
       </div>
       <div className='card-body bg-slate-50'>
-        {cardBody &&
-          cardBody.map((item, i) => {
+        {items &&
+          items.map((item, i) => {
             return (
               <div key={i} className='text-center'>
-                {item}
+                {item.name}
               </div>
             );
           })}
       </div>
+
       {/* <h2>hello world</h2> */}
       <button className='btn rounded-none'>expand</button>
     </div>
   );
 };
 
-const storeCardData = {
-  cardTitle: "store name",
-
-  // cardTitle: ,
-  cardBody: [
-    "store items list",
-    "store items list",
-    "store items list",
-    "store items list",
-  ],
+const defaultStoreCardData = {
+  store: {
+    id: "nan",
+    name: "Store Name",
+    userId: "nan",
+  },
 };
 
 export const Home = () => {
   const { handleLogout, user } = useAuthProvider();
+  const { stores } = useAppProvider();
+  console.log(stores);
   const name = user?.name;
   const subTitle: string = `Welcome ${name} to your home page!`;
-
+  const title: string = "Ready Set Go!";
   const navigate = useNavigate();
+
   return (
     <>
       <div className='card w-96 bg-base-100 shadow-xl m-auto'>
@@ -56,12 +65,12 @@ export const Home = () => {
           <h2 className='text-md'>{subTitle}</h2>
         </div>
         <div className='card-body'>
-          {
-            <StoreCard
-              cardTitle='store name'
-              cardBody={storeCardData.cardBody}
-            />
-          }
+          {user !== null && user !== undefined ? (
+            stores &&
+            stores.map((store) => <StoreCard store={store} key={store.id} />)
+          ) : (
+            <StoreCard store={defaultStoreCardData.store} />
+          )}
         </div>
         <div className='flex justify-between'>
           <button
@@ -79,9 +88,6 @@ export const Home = () => {
           >
             Home
           </button>
-          {/* <button className='btn btn-outline rounded-none btn-secondary'>
-            New
-          </button> */}
           {/* Open the modal using document.getElementById('ID').showModal() method */}
           <button
             className='btn btn-outline rounded-none btn-info'

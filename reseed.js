c2452095459c478ab04919161670e0e453d80740
Tyson <deppravacion@gmail.com>
2024-06-14 19:25:05 -0600

    Refactor: adopt Id pattern to avoid bugs

--- reseed.js
+++ reseed.js
@@ -3,13 +3,13 @@ import { writeFileSync } from "fs";
 const db = {
   users: [
     {
-      id: "01",
+      id: "01aa",
       name: "Robert Paulson",
       email: "bobby.paulson@readysetgo.com",
       password: "pass",
     },
     {
-      id: "02",
+      id: "02bb",
       name: "Tester",
       email: "test@test.com",
       password: "pass",
@@ -19,37 +19,37 @@ const db = {
     {
       id: "01",
       name: "garageFridge",
-      userId: "01",
+      userId: "01aa",
     },
     {
-      id: "02",
+      id: "022c",
       name: "lx470",
-      userId: "01",
+      userId: "01aa",
     },
   ],
   items: [
     {
-      id: "01",
+      id: "01a1",
       name: "shovel",
       image:
         "https://plus.unsplash.com/premium_photo-1680658496041-f7575066cec2?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       description: "short shovel",
       quantity: "1",
       minQuantity: "1",
-      storeId: "02",
+      storeId: "022c",
     },
     {
-      id: "02",
+      id: "02b1",
       name: "recoveryGear",
       image:
         "https://media.istockphoto.com/id/2101625741/photo/fastenings-for-transporting-goods-straps-steel-shackles-and-ratchet-locks.webp?b=1&s=170667a&w=0&k=20&c=w2DTHxBsWkUjQWtUivHmuNzDgtmPAqQBIhuY5vZFdm0=",
       description: "straps, shackles, etc",
       quantity: "1",
       minQuantity: "1",
-      storeId: "02",
+      storeId: "022c",
     },
     {
-      id: "03",
+      id: "03c1",
       name: "babyMilk    ",
       image: "babyMilk.jpg",
       description: "frozen breast milk",
@@ -60,11 +60,11 @@ const db = {
   ],
   favorites: [
     {
-      itemId: "01",
+      itemId: "01a1",
       id: "28ab",
     },
     {
-      itemId: "02",
+      itemId: "02b1",
       id: "a0b5",
     },
   ],
